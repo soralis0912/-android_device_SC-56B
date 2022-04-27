115c30b6b76a86c210cbedfb282c432283d2d14b
そらりす <soralis.nem@gmail.com>
2022-04-27 17:53:20 +0900

    SC-56B: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SC-56B Script version: 2.2.0

---
+++ omni_SC-56B.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from SC-56B device
+$(call inherit-product, device/samsung/SC-56B/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := SC-56B
+PRODUCT_NAME := omni_SC-56B
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SC-56B
+PRODUCT_MANUFACTURER := samsung
